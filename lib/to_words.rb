7554960e55527df8b02bfb92a12408fd4b6a7f8d
Ahmed Ali <ahmedrali2@gmail.com>
2015-02-14 09:26:17 +0200

    - test arabic words

--- lib/to_words.rb
+++ lib/to_words.rb
@@ -2,8 +2,110 @@ require "to_words/version"
 
 module ToWords
 
-  UNDER_HUNDRED = {""=>"", 0=>"zero", 1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine", 10=>"ten", 11=>"eleven", 12=>"twelve", 13=>"thirteen", 14=>"fourteen", 15=>"fifteen", 16=>"sixteen", 17=>"seventeen", 18=>"eighteen", 19=>"nineteen", 20=>"twenty", 21=>"twenty one", 22=>"twenty two", 23=>"twenty three", 24=>"twenty four", 25=>"twenty five", 26=>"twenty six", 27=>"twenty seven", 28=>"twenty eight", 29=>"twenty nine", 30=>"thirty", 31=>"thirty one", 32=>"thirty two", 33=>"thirty three", 34=>"thirty four", 35=>"thirty five", 36=>"thirty six", 37=>"thirty seven", 38=>"thirty eight", 39=>"thirty nine", 40=>"forty", 41=>"forty one", 42=>"forty two", 43=>"forty three", 44=>"forty four", 45=>"forty five", 46=>"forty six", 47=>"forty seven", 48=>"forty eight", 49=>"forty nine", 50=>"fifty", 51=>"fifty one", 52=>"fifty two", 53=>"fifty three", 54=>"fifty four", 55=>"fifty five", 56=>"fifty six", 57=>"fifty seven", 58=>"fifty eight", 59=>"fifty nine", 60=>"sixty", 61=>"sixty one", 62=>"sixty two", 63=>"sixty three", 64=>"sixty four", 65=>"sixty five", 66=>"sixty six", 67=>"sixty seven", 68=>"sixty eight", 69=>"sixty nine", 70=>"seventy", 71=>"seventy one", 72=>"seventy two", 73=>"seventy three", 74=>"seventy four", 75=>"seventy five", 76=>"seventy six", 77=>"seventy seven", 78=>"seventy eight", 79=>"seventy nine", 80=>"eighty", 81=>"eighty one", 82=>"eighty two", 83=>"eighty three", 84=>"eighty four", 85=>"eighty five", 86=>"eighty six", 87=>"eighty seven", 88=>"eighty eight", 89=>"eighty nine", 90=>"ninety", 91=>"ninety one", 92=>"ninety two", 93=>"ninety three", 94=>"ninety four", 95=>"ninety five", 96=>"ninety six", 97=>"ninety seven", 98=>"ninety eight", 99=>"ninety nine", 100=>"one hundred"}
-  DIVISIONS = ["", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintrillion"]
+  UNDER_HUNDRED = {  "" => "", 
+                     0 => "صفر", 
+                     1 => "واحد",
+                     2 => "اثنين",
+                     3 => "ثلاثة",
+                     4 => "أربعة",
+                     5 => "خمسة",
+                     6 => "ست",
+                     7 => "سبعة",
+                     8 => "ثمانية",
+                     9 => "تسعة",
+                     10 => "عشرة",
+                     11 => "أحد عشر",
+                     12 => "اثنا عشر",
+                     13 => "ثلاثة عشر",
+                     14 => "أربعة عشر",
+                     15 => "خمسة عشر",
+                     16 => "ستة عشر",
+                     17 => "سبعة عشر",
+                     18 => "ثمانية عشر",
+                     19 => "تسعة عشر",
+                     20 => "عشرين",
+                     21 => "واحد وعشرين",
+                     22 => "اثنين وعشرين",
+                     23 => "ثلاثة وعشرين",
+                     24 => "أربع وعشرين",
+                     25 => "خمسة وعشرين",
+                     26 => "ستة وعشرين",
+                     27 => "سبعة وعشرين",
+                     28 => "ثمانية وعشرين",
+                     29 => "تسعة وعشرين",
+                     30 => "ثلاثين",
+                     31 => "واحد وثلاثون",
+                     32 => "اثنين وثلاثين",
+                     33 => "ثلاثة وثلاثين",
+                     34 => "أربعة وثلاثين",
+                     35 => "خمسة وثلاثين",
+                     36 => "ستة وثلاثين",
+                     37 => "سبعة وثلاثين",
+                     38 => "ثمانية وثلاثين",
+                     39 => "تسعة وثلاثين",
+                     40 => "أربعين",
+                     41 => "احد وأربعين",
+                     42 => "اثنين وأربعين",
+                     43 => "ثلاثة وأربعون",
+                     44 => "أربعة وأربعون",
+                     45 => "خمسة وأربعين",
+                     46 => "ستة وأربعين",
+                     47 => "سبعة وأربعون",
+                     48 => "ثمان وأربعين",
+                     49 => "تسعة وأربعون",
+                     50 => "خمسون",
+                     51 => "واحد وخمسون",
+                     52 => "إثنان وخمسون",
+                     53 => "ثلاثة وخمسون",
+                     54 => "أربعة وخمسين",
+                     55 => "خمسة وخمسون",
+                     56 => "ستة وخمسين",
+                     57 => "سبعة وخمسين",
+                     58 => "ثمانية وخمسون",
+                     59 => "تسعة وخمسون",
+                     60 => "ستين",
+                     61 => "واحد وستون",
+                     62 => "اثنان وستون",
+                     63 => "ثلاث وستون",
+                     64 => "أربعة وستين",
+                     65 => "خمسة وستين",
+                     66 => "ستة وستين",
+                     67 => "سبع وستون",
+                     68 => "ثمانية وستون",
+                     69 => "تسعة وستين",
+                     70 => "سبعون",
+                     71 => "واحد وسبعون",
+                     72 => "اثنان وسبعون",
+                     73 => "ثلاثة وسبعون",
+                     74 => "اربعة وسبعون",
+                     75 => "خمسة وسبعين",
+                     76 => "ستة وسبعين",
+                     77 => "سبعة وسبعون",
+                     78 => "ثمانية وسبعين",
+                     79 => "تسعة وسبعون",
+                     80 => "ثمانين",
+                     81 => "واحد وثمانين", 
+                     82 => "اثنين وثمانين", 
+                     83 => "ثلاثة وثمانين",
+                     84 => "أربعة وثمانون",
+                     85 => "خمسة وثمانين",
+                     86 => "ستة وثمانون",
+                     87 => "سبعة وثمانون",
+                     88 => "ثمانية وثمانين",
+                     89 => "تسعة وثمانون",
+                     90 => "تسعين",
+                     91 => "واحد وتسعون", 
+                     92 => "اثنان وتسعون", 
+                     93 => "ثلاثة وتسعون", 
+                     94 => "أربعة وتسعون", 
+                     95 => "خمسة وتسعون", 
+                     96 => "ستة وتسعون",
+                     97 => "سبعة وتسعين",
+                     98 => "ثمانية وتسعين",
+                     99 => "تسعة وتسعين", 
+                     100 => "مائة"
+                  }
+  DIVISIONS = ["", "ألف", "مليون", "مليار", "تريليون", "كوادريليون", "كوينتيليون"]
 
   def to_words
     num = self.to_i
@@ -17,15 +119,15 @@ module ToWords
       result << temp_result + " " + DIVISIONS[counter] + " " if temp_result != ''
       counter += 1
     end
-    return sign + result.reverse.join(", ").rstrip
+    return sign + result.reverse.join("و ").rstrip
   end
 
   def result_below_one_thousand(num, counter)
     hundred, remaining = num.divmod(100)
-    return UNDER_HUNDRED[hundred] + " Hundred " + UNDER_HUNDRED[remaining]     if hundred != 0 && remaining != 0 && counter != 0
-    return UNDER_HUNDRED[hundred] + " Hundred and " + UNDER_HUNDRED[remaining] if hundred != 0 && remaining != 0
+    return UNDER_HUNDRED[hundred] + " مائة و " + UNDER_HUNDRED[remaining]     if hundred != 0 && remaining != 0 && counter != 0
+    return UNDER_HUNDRED[hundred] + " مائة و " + UNDER_HUNDRED[remaining] if hundred != 0 && remaining != 0
     return UNDER_HUNDRED[remaining]                                            if hundred == 0 && remaining != 0
-    return UNDER_HUNDRED[hundred] + " Hundred "                                if hundred != 0 && remaining == 0
+    return UNDER_HUNDRED[hundred] + " مائة "                                if hundred != 0 && remaining == 0
     return ''
   end
 
